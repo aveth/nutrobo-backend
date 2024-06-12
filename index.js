b05cce366e831eeeaa63059265b5ec9cd24c3b0d
Avais Sethi <avais.sethi@gmail.com>
2024-06-12 16:46:56 -0400

    spacing

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ process.on('uncaughtException', function(err) {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 app.get('/', (req, res) => {
     res.send(`Welcome to the Nutrobo API! The current version is ${pjson.version}.`);
 });
